5f3dcdd387a833fed54a74265281e87901ab9cff
lee seungwon <tmddnjs2385@naver.com>
2020-11-25 03:12:06 +0900

    Class Components and state

--- src/App.js
+++ src/App.js
@@ -1,74 +1,29 @@
-
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({name, picture, rating}){
-  return (
-    <div>
-         <h1> i love {name}</h1>
-         <h4>{rating}/5.0</h4>
-         <img src={picture} alt={name}/>
-    </div>
-  )
-}
+class App extends React.Component{
+  state={
+    count: 0
+  };
 
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating: 5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating: 4.5
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating: 4.8
-  },
-  { id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating: 4.9
-  },
-  { id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating: 5.0
+  add=()=>{
+    console.log("add");
   }
-];
 
-function renderFood(dish){
-  console.log();
-}
+  minus=()=>{
+    console.log("minus");
+  }
 
-Food.propTypes={
-
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating:  PropTypes.string.isRequired
-
-}
-
-function App(){
-  return (
-    <div>
-      {
-        foodILike.map(function (dish){return <Food key={dish.id} name={dish.name } picture={dish.image} rating={dish.rating}/>})
-      }
+  render(){
+    return(
+     <div>
+       <h1>The number is: {this.state.count}</h1>
+      <h1>Im a class component</h1>;
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-
-  )
+    );
+  }
 }
 
-export default App;
+export default App; 
