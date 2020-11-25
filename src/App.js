8a4c1c5b22c8f6f3c9193d273283460ecb799b2c
lee seungwon <tmddnjs2385@naver.com>
2020-11-26 02:49:31 +0900

    Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -7,47 +7,33 @@ class App extends Component {
 
   }
 
-  componentDidMount(){
-    console.log("component rendered");
+  state={
+    isLoading: true,
+    movies:[]
   }
 
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
+  
+  componentDidMount() {
 
-  componentWillUnmount(){
-    console.log("what the Fu....?");
+    setTimeout(()=>{
+      this.setState({
+        isLoading:false
+      });
+    },3000);
+
   }
 
  
-
-  state={
-    count: 0
-  };
-
-  add=()=>{
-    console.log("add");
-    this.setState(current=>({count:current.count+1}));
-   
-  }
-
-  minus=()=>{
-    console.log("minus");
-    this.setState(current=>({count:current.count-1}));
-    
-  }
-
   render() {
 
-    console.log("I'm rendering");
+    const {isLoading}=this.state;
+
     return (
       <div>
-
-        <h1>The number is: {this.state.count}</h1>
-        <h1>I'm a class component</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-
+        {
+          isLoading ? "Loading...": "We are ready"
+        }
+        
       </div>
     );
   }
