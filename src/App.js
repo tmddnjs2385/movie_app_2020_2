693ff5f3f0e91ef2a0e12f6790676d7f0b66291c
lee seungwon <tmddnjs2385@naver.com>
2020-11-26 01:59:24 +0900

    Class components and State

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class App extends React.Component{
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
   state={
     count: 0
   };
 
   add=()=>{
     console.log("add");
+   
   }
 
   minus=()=>{
     console.log("minus");
+    
   }
 
-  render(){
-    return(
-     <div>
-       <h1>The number is: {this.state.count}</h1>
-      <h1>Im a class component</h1>;
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+  render() {
+    return (
+      <div>
+
+        <h1>The number is: {this.state.count}</h1>
+        <h1>I'm a class component</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+
+      </div>
     );
   }
 }
 
-export default App; 
+App.propTypes = {
+
+};
+
+export default App;
