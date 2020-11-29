669534dee9fb241ced2ac848757239c6ef49bc81
lee seungwon <tmddnjs2385@naver.com>
2020-11-29 23:20:25 +0900

    Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Movie from "./Movie"
+import Movie from "./Movie";
 import axios from "axios";
+import "./App.css"
 
 class App extends Component {
   constructor(props) {
     super(props);
-
   }
 
   state={
-  
     isLoading: true,
     movies:[]
   }
@@ -24,39 +23,36 @@ class App extends Component {
         }
       }
     }=await axios.get("https://yts-proxy.now.sh/list_movies.json");
-
     console.log(movies);
-
     this.setState({movies:movies, isLoading:false});
   }
 
-  
-
   componentDidMount() {
-
   this.getMovies();
-
   }
 
-  
-
   render() {
-
     const {isLoading, movies}=this.state;
-
     return (
+      <section class="container">
+      
+        { isLoading ?  <div class="loader">
 
-      <div>
-        { isLoading ?  "Loading..." : movies.map(movie=>(
+          <span class="loader__text">loading...</span>
+
+        </div> : movies.map(movie=>(
+
           <Movie key={movie.id}
            id={movie.id} 
            year={movie.year} 
-           title={movie.title} 
+           title={movie.title}
            summary={movie.summary}
-            poster={movie.medium_cover_image}></Movie>
+          poster={movie.medium_cover_image}>
+          </Movie>
+
         ))}
 
-      </div>
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie(){
-    return <h1>
-        
-    </h1>
+function Movie({id, year, title, summary, poster}){
+    return (
+    <div class="movie">
+
+        <img src={poster} alt={title} title={title}/>
+        <h3 class="movie__title" style={{backgroundColor:"red"}}>{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+
+    </div>
+    )
 }
 
 Movie.propTypes={
+
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
