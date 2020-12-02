c9529cd2bd11947e8de3564fa920c1e5cb02f124
tmddnjs2385 <tmddnjs2385@naver.com>
2020-12-03 03:30:09 +0900

    Building the Navigation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import {HashRouter, Route} from "react-router-dom";
 import About  from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+
 
 function App(){
   return (
     <HashRouter>
+      
+     <Navigation></Navigation>
       <Route path="/" exact={true} component={Home}/>
       <Route path="/about" component={About}/>
-      
+    
       
     </HashRouter>
   );
